db1cf23e4c33b40b1ab2846ac0ca260f72d65d38
theo rakotoson <rakotoson.theo@gmail.com>
2021-08-17 15:52:23 +0200

    rangement

---
+++ JavaScript/index.js
@@ -0,0 +1,73 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const apiurl = "https://teddies-api.herokuapp.com/api";
+  
+  const cards = document.querySelector('#cards');
+  
+
+
+  fetch(apiurl + "/cameras")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+
+      data.forEach((element) => {
+        console.log(element.name);
+        
+        
+        createElement(element);
+      });
+    })
+    .catch((error) => {
+      alert("erreur");
+    });
+        
+
+  function createElement(element) {
+    // creations éléments
+    let div = document.createElement("a");
+    let price = document.createElement("p");
+    let name = document.createElement("p");
+    let description = document.createElement("p");
+    // let lenses = document.createElement("p");
+    let img = document.createElement("img");
+    // let link = document.createElement("a");
+
+    // image
+    img.innerHTML = null;
+    // le prix
+    price.innerHTML = element.price+" €";
+    // le nom
+    name.innerHTML = element.name;
+    // description
+    description.innerHTML = element.description;
+    // lenses
+    // lenses.innerHTML = element.lenses;
+    // links
+    // link.innerHTML = "oui";
+
+    // attribution classes
+    div.setAttribute("class", "div", "href");
+    div.setAttribute("href","item.html"+ "?id=" + element._id);
+    price.setAttribute("class", "price");
+    name.setAttribute("class", "name");
+    img.setAttribute("class", "img");
+    img.setAttribute("src", element.imageUrl);
+    img.setAttribute("alt", element.name);
+    // lenses.setAttribute("class", "lenses");
+    description.setAttribute("class", "description");
+    // link.setAttribute("class","link");
+    // link.setAttribute("href","item.html"+ "?id=" + element._id);
+
+    // ordre DOM
+    cards.prepend(div);
+    // div.prepend(link);
+    div.prepend(description);
+    // div.prepend(lenses);
+    div.prepend(price);
+    div.prepend(name);
+    div.prepend(img);
+    
+  }
+  
+});
+
