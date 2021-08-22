9fbb8032b9ae36ef61b910a33ba0537f74ffd081
theo rakotoson <rakotoson.theo@gmail.com>
2021-08-22 17:06:38 +0200

    modification js creation des cards

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
   
   const cards = document.querySelector('.cards_container');
   
+  
 
 
   fetch(apiurl + "/cameras")
@@ -23,44 +24,26 @@ document.addEventListener("DOMContentLoaded", (event) => {
         
 
   function createElement(element) {
-    // creations éléments
-    let div = document.createElement("a");
-    let price = document.createElement("p");
-    let name = document.createElement("p");
-    let description = document.createElement("p");
-    // let lenses = document.createElement("p");
-    let img = document.createElement("img");
-    // let link = document.createElement("a");
-
-    // image
-    img.innerHTML = null;
-    // le prix
-    price.innerHTML = element.price+" €";
-    // le nom
-    name.innerHTML = element.name;
-    // description
-    description.innerHTML = element.description;
-   
-
-    // attribution classes
-    div.setAttribute("class", "cards", "href");
-    div.setAttribute("href","item.html"+ "?id=" + element._id);
-    price.setAttribute("class", "price");
-    name.setAttribute("class", "name");
-    img.setAttribute("class", "img");
-    img.setAttribute("src", element.imageUrl);
-    img.setAttribute("alt", element.name);
-    description.setAttribute("class", "description");
-    
-
-    // ordre DOM
-    cards.prepend(div);
-    div.prepend(description);
-    div.prepend(price);
-    div.prepend(name);
-    div.prepend(img);
-    
+    // creation des cards 
+     cards.innerHTML += `
+     <div class="cards">
+     <a href="item.html?_id=${element._id}"><img class=img src="${element.imageUrl}" alt=""></a>
+     <div class=text>
+        <p>${element.name}</p>
+        <p>${element.price}</p>
+     </div>
+     <div class="description">
+     <p>${element.description}</p>
+     </div>
+     <input type="button" onclick="window.location.href='item.html?_id=${element._id}';" value="achat"></input>
+    </div>`;
   }
+ 
+
+
+  
+
+
   
 });
 
